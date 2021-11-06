7518124b9ad09e7b5fe1ec08e3750a85761435df
mingsongli <limingsonglms@qq.com>
2021-11-06 20:29:49 +0800

    v2.4 add Milankovitch Calculator

--- code/guicode/LODGUI.m
+++ code/guicode/LODGUI.m
@@ -54,7 +54,7 @@ function LODGUI_OpeningFcn(hObject, eventdata, handles, varargin)
 handles.MonZoom = varargin{1}.MonZoom;
 handles.sortdata = varargin{1}.sortdata;
 
-set(gcf,'Name','Acycle: Lenght-of-day & Day-of-year')
+set(gcf,'Name','Acycle: Milankovitch Calculator')
 set(gcf,'position',[0.4,0.55,0.313,0.289]* handles.MonZoom) % set position
 set(0,'Units','normalized') % set units as normalized
 set(gcf,'units','norm') % set location
@@ -98,7 +98,7 @@ set(handles.text4, 'Visible','on')
 set(handles.text6, 'Visible','on')
 set(handles.text7, 'Visible','on')
 
-set(handles.text8, 'String','will calculate length of day and days in a year: -10, 0, 10, ... , 250 Ma')
+set(handles.text8, 'String','will calculate: -10, 0, 10, ... , 250 Ma')
 
 handles.listbox_acmain = varargin{1}.listbox_acmain; % save path
 handles.edit_acfigmain_dir = varargin{1}.edit_acfigmain_dir;
@@ -130,70 +130,263 @@ function pushbutton1_Callback(hObject, eventdata, handles)
 % handles    structure with handles and user data (see GUIDATA)
 single0 = get(handles.radiobutton1,'Value');
 single1 = get(handles.radiobutton2,'Value');
-if single0 == 1
-    T1 = str2double(get(handles.edit1,'string'));
-    if T1 >=4500 || T1<=-4500
-        if T1 >=4500
-            errordlg('Given age is too large')
-        else
-            errordlg('Given age is too small')
-        end
-    else
-        
-        [lod, doy] = lodla04(T1/-1000);
-        
-        name1 = ['LOD_',num2str(T1),'Ma.txt'];
-        name2 = ['LOD_DOY_',num2str(T1),'Ma.txt'];
-        CDac_pwd
-        dlmwrite(name1, [T1,lod], 'delimiter', ',', 'precision', 9);
-        dlmwrite(name2, [T1,doy], 'delimiter', ',', 'precision', 9);
-        d = dir; %get files
-        set(handles.listbox_acmain,'String',{d.name},'Value',1) %set string
-        refreshcolor;
-        cd(pre_dirML); % return to matlab view folder
+model = get(handles.popupmenu1,'Value');
+if model == 1
+    if single0 == 1
+        % single time
+        T1 = str2double(get(handles.edit1,'string'));
+        if T1 >=4500 || T1<=-4500
+            if T1 >=4500
+                errordlg('Given age is too large')
+            else
+                errordlg('Given age is too small')
+            end
+        else            
+            [daymin, daymax,amin, amax, kmin, kmax, obmin,...
+                obmax, o1min, o1max, p1min, p1max, p2min, p2max, ...
+                p3min, p3max, p4min, p4max] = MilankovitchCal(T1);
+            
+            % write out distance range
+            distance = 0.5*( amax + amin );
+            distsigma = 0.5*( amax - amin);
+            %accuracy = decimalPlaces(distsigma);
+            %disp(['Earth-Moon Distance : ', num2str(distance), ' +/- ', num2str(distsigma),' km'])
 
-        figure;
-        set(gcf,'Name','Acycle: Lenght-of-day & Day-of-year | plot')
-        set(gcf,'color','w');
-        subplot(2,1,1)
-        plot(T1,lod,'-ko')
-        ylabel('length of day (hour)')
-        subplot(2,1,2)
-        plot(T1,doy,'-ko')
-        xlabel('Age (Ma)')
-        ylabel('stellar day of year')
+            % write out day-length range
+            day = 0.5*( daymax + daymin );
+            daysigma = 0.5*( daymax - daymin );
+            %accuracy = decimalPlaces(daysigma);
+            %disp(['Earth Day : ', num2str(day), ' +/- ', num2str(daysigma),' hours'])
+
+            % write out obliquity range
+            obliq = 0.5*( obmax + obmin );
+            obsigma = 0.5*( obmax - obmin);
+            %accuracy = decimalPlaces(obsigma);
+            %disp(['Earth Axis Mean Obliquity : ', num2str(obliq), ' +/- ', num2str(obsigma),' degrees'])
+
+            % write out precession range
+            kkymin = 360.0*3.6/kmax;
+            kkymax = 360.0*3.6/kmin;
+            prec = 0.5*( kkymax + kkymin );
+            precsigma = 0.5*( kkymax - kkymin);
+            %accuracy = decimalPlaces(precsigma);
+            %disp(['Earth Axis Precession Period : ', num2str(prec), ' +/- ', num2str(precsigma),' kyr'])
+
+            % write out obliquity cycle periods
+            o1 = 0.5*( o1max + o1min );
+            o1sigma = 0.5*( o1max - o1min );
+            %accuracy = decimalPlaces(p1sigma);
+            %disp('Main Obliquity Period : (kyr)')
+            %disp([num2str(o1), ' +/- ', num2str(o1sigma)])
+
+            % write out climatic precession periods
+            p1 = 0.5*( p1max + p1min );
+            p1sigma = 0.5*( p1max - p1min );
+            %accuracy = decimalPlaces(p1sigma);
+            %disp('Climatic Precession Periods : (kyr)')
+            %disp([num2str(p1), ' +/- ', num2str(p1sigma)])
+
+            p2 = 0.5*( p2max + p2min );
+            p2sigma = 0.5*( p2max - p2min );
+            %accuracy = decimalPlaces(p2sigma);
+            %disp([num2str(p2), ' +/- ', num2str(p2sigma)])
+
+            p3 = 0.5*( p3max + p3min );
+            p3sigma = 0.5*( p3max - p3min );
+            %accuracy = decimalPlaces(p3sigma);
+            %disp([num2str(p3), ' +/- ', num2str(p3sigma)])
+
+            p4 = 0.5*( p4max + p4min );
+            p4sigma = 0.5*( p4max - p4min );
+            %accuracy = decimalPlaces(p4sigma);
+            %disp([num2str(p4), ' +/- ', num2str(p4sigma)])
+            
+            prompt = {...
+                'Earth-Moon Distance (x1000 km)'; ...
+                'Earth Day (hours)';...
+                'Earth Axis Mean Obliquity (degrees)'; ...
+                'Earth Axis Precession Period (kyr)';...
+                'Main Obliquity Period (kyr)';...
+                'Climatic Precession Periods #1 (kyr)';...
+                'Climatic Precession Periods #2 (kyr)';...
+                'Climatic Precession Periods #3 (kyr)';...
+                'Climatic Precession Periods #4 (kyr)';...
+                'Details in: Waltham, D., 2015. JSR. doi: 10.2110/jsr.2015.66'};
+            dlg_title = 'Milankovitch Calculator';
+            num_lines = 1;
+            defaultans = {...
+                [num2str(distance), ' +/- ', num2str(distsigma)],...
+                [num2str(day), ' +/- ', num2str(daysigma)],...
+                [num2str(obliq), ' +/- ', num2str(obsigma)],...
+                [num2str(prec), ' +/- ', num2str(precsigma)],...
+                [num2str(o1), ' +/- ', num2str(o1sigma)],...
+                [num2str(p1), ' +/- ', num2str(p1sigma)],...
+                [num2str(p2), ' +/- ', num2str(p2sigma)],...
+                [num2str(p3), ' +/- ', num2str(p3sigma)],...
+                [num2str(p4), ' +/- ', num2str(p4sigma)],...
+                'https://davidwaltham.com/wp-content/uploads/2014/01/Milankovitch.html'};
+            options.Resize='on';
+            answer = inputdlg(prompt,dlg_title,num_lines,defaultans,options);
+                if ~isempty(answer)
+                end
+        end
+    elseif single1 == 1
+        % time series
+        age1 = str2double(get(handles.edit1,'string'));
+        age2 = str2double(get(handles.edit2,'string'));
+        step = str2double(get(handles.edit3,'string'));
+        T1 = age1:step:age2;
+        T1 = T1';
+        T1n = length(T1);
+        if isempty(T1) == 0
+            for ti = 1:T1n
+                age = T1(ti);
+                [daymin, daymax,amin, amax, kmin, kmax, obmin,obmax, o1min, o1max, ...
+                    p1min, p1max, p2min, p2max, p3min, p3max, p4min, p4max] = MilankovitchCal(age);
+                calmi(ti,:) = [daymin, daymax, amin, amax, kmin, kmax, obmin, obmax, o1min, o1max, ...
+                    p1min, p1max, p2min, p2max, p3min, p3max, p4min, p4max];
+            end
+            
+            figure;
+            set(gcf,'Name','Acycle: Milankovitch Calculator | Plot')
+            set(gcf,'color','white')
+            subplot(3,2,1); hold on;
+            plot(T1,calmi(:,3),'k--')
+            plot(T1,calmi(:,4),'k--')
+            plot(T1,(calmi(:,3) + calmi(:,4))/2,'r-','LineWidth',1)
+            ylabel('Earth-Moon Distance (x1000 km)')
+            xlim([age1, age2])
+            
+            subplot(3,2,2); hold on;
+            plot(T1,calmi(:,1),'k--')
+            plot(T1,calmi(:,2),'k--')
+            plot(T1,(calmi(:,1) + calmi(:,2))/2,'r-','LineWidth',1)
+            ylabel('Earth Day (hours)')
+            xlim([age1, age2])
+                      
+            subplot(3,2,4); hold on;
+            plot(T1,360.0*3.6./calmi(:,5),'k--')
+            plot(T1,360.0*3.6./calmi(:,6),'k--')
+            plot(T1,0.5*( 360.0*3.6./calmi(:,5) + 360.0*3.6./calmi(:,6)),'r-','LineWidth',1)
+            ylabel('Earth Axis Precession Period (kyr)')
+            xlim([age1, age2])
+            
+            subplot(3,2,3); hold on;
+            plot(T1,calmi(:,7),'k--')
+            plot(T1,calmi(:,8),'k--')
+            plot(T1,(calmi(:,7) + calmi(:,8))/2,'r-','LineWidth',1)
+            ylabel('Earth Axis Mean Obliquity (degrees)')
+            xlim([age1, age2])
+            
+            subplot(3,2,5); hold on;
+            plot(T1,calmi(:,9),'k--')
+            plot(T1,calmi(:,10),'k--')
+            plot(T1,(calmi(:,9) + calmi(:,10))/2,'r-','LineWidth',1)
+            ylabel('Main Obliquity Period (kyr)')
+            xlabel('Age (Ma)')
+            xlim([age1, age2])
+            
+            subplot(3,2,6); hold on;
+            plot(T1,calmi(:,11),'k--')
+            plot(T1,calmi(:,12),'k--')
+            plot(T1,(calmi(:,11) + calmi(:,12))/2,'k-','LineWidth',1)
+            
+            plot(T1,calmi(:,13),'g--')
+            plot(T1,calmi(:,14),'g--')
+            plot(T1,(calmi(:,13) + calmi(:,14))/2,'g-','LineWidth',1)
+            
+            plot(T1,calmi(:,15),'b--')
+            plot(T1,calmi(:,16),'b--')
+            plot(T1,(calmi(:,15) + calmi(:,16))/2,'b-','LineWidth',1)
+            
+            plot(T1,calmi(:,17),'r--')
+            plot(T1,calmi(:,18),'r--')
+            plot(T1,(calmi(:,17) + calmi(:,18))/2,'r-','LineWidth',1)
+            xlabel('Age (Ma)')
+            ylabel('Climatic Precession Periods (kyr)','LineWidth',1)
+            xlim([age1, age2])
+            
+            name1 = ['CalMi_',num2str(age1),'_',num2str(age2),'-Ma-step_',num2str(step),'.txt'];
+            CDac_pwd
+            disp('age, daymin, daymax, amin, amax, kmin, kmax, obmin, obmax, o1min, o1max, p1min, p1max, p2min, p2max, p3min, p3max, p4min, p4max')
+            dlmwrite(name1, [T1,calmi], 'delimiter', ',', 'precision', 9);
+            d = dir; %get files
+            set(handles.listbox_acmain,'String',{d.name},'Value',1) %set string
+            refreshcolor;
+            cd(pre_dirML); % return to matlab view folder
+        else
+            errordlg('Input age error !')
+        end
     end
-elseif single1 == 1
-    age1 = str2double(get(handles.edit1,'string'));
-    age2 = str2double(get(handles.edit2,'string'));
-    step = str2double(get(handles.edit3,'string'));
-    T1 = age1:step:age2;
-    T1 = T1';
-    if isempty(T1) == 0
-        [lod, doy] = lodla04(T1/-1000);
-        
-        name1 = ['LOD_',num2str(age1),'_',num2str(step),'_',num2str(age2),'Ma.txt'];
-        name2 = ['LOD_DOY_',num2str(age1),'_',num2str(step),'_',num2str(age2),'Ma.txt'];
-        CDac_pwd
-        dlmwrite(name1, [T1,lod], 'delimiter', ',', 'precision', 9);
-        dlmwrite(name2, [T1,doy], 'delimiter', ',', 'precision', 9);
-        d = dir; %get files
-        set(handles.listbox_acmain,'String',{d.name},'Value',1) %set string
-        refreshcolor;
-        cd(pre_dirML); % return to matlab view folder
-        
-        figure;
-        set(gcf,'Name','Acycle: Lenght-of-day & Day-of-year | plot')
-        set(gcf,'color','w');
-        subplot(2,1,1)
-        plot(T1,lod,'-ko')
-        ylabel('length of day (hours)')
-        subplot(2,1,2)
-        plot(T1,doy,'-ko')
-        xlabel('Age (Ma)')
-        ylabel('stellar day of year')
-    else
-        errordlg('Input age error !')
+elseif model == 2
+    if single0 == 1
+        % single time
+        T1 = str2double(get(handles.edit1,'string'));
+        if T1 >=4500 || T1<=-4500
+            if T1 >=4500
+                errordlg('Given age is too large')
+            else
+                errordlg('Given age is too small')
+            end
+        else
+
+            [lod, doy] = lodla04(T1/-1000);
+
+            name1 = ['LOD_',num2str(T1),'Ma.txt'];
+            name2 = ['LOD_DOY_',num2str(T1),'Ma.txt'];
+            CDac_pwd
+            dlmwrite(name1, [T1,lod], 'delimiter', ',', 'precision', 9);
+            dlmwrite(name2, [T1,doy], 'delimiter', ',', 'precision', 9);
+            d = dir; %get files
+            set(handles.listbox_acmain,'String',{d.name},'Value',1) %set string
+            refreshcolor;
+            cd(pre_dirML); % return to matlab view folder
+
+            figure;
+            set(gcf,'Name','Acycle: Lenght-of-day & Day-of-year | plot')
+            set(gcf,'color','w');
+            subplot(2,1,1)
+            plot(T1,lod,'-ko')
+            ylabel('length of day (hour)')
+            subplot(2,1,2)
+            plot(T1,doy,'-ko')
+            xlabel('Age (Ma)')
+            ylabel('stellar day of year')
+        end
+    elseif single1 == 1
+        % time series
+        age1 = str2double(get(handles.edit1,'string'));
+        age2 = str2double(get(handles.edit2,'string'));
+        step = str2double(get(handles.edit3,'string'));
+        T1 = age1:step:age2;
+        T1 = T1';
+        if isempty(T1) == 0
+            [lod, doy] = lodla04(T1/-1000);
+
+            %name1 = ['LOD_',num2str(age1),'_',num2str(step),'_',num2str(age2),'Ma.txt'];
+            name1 = ['LOD-',num2str(age1),'_',num2str(age2),'_Ma-step_',num2str(step),'.txt'];
+            %name2 = ['LOD_DOY_',num2str(age1),'_',num2str(step),'_',num2str(age2),'Ma.txt'];
+            CDac_pwd
+            dlmwrite(name1, [T1,lod,doy], 'delimiter', ',', 'precision', 9);
+            %dlmwrite(name2, [T1,doy], 'delimiter', ',', 'precision', 9);
+            d = dir; %get files
+            set(handles.listbox_acmain,'String',{d.name},'Value',1) %set string
+            refreshcolor;
+            cd(pre_dirML); % return to matlab view folder
+
+            figure;
+            set(gcf,'Name','Acycle: Lenght-of-day & Day-of-year | plot')
+            set(gcf,'color','w');
+            subplot(2,1,1)
+            plot(T1,lod,'-ko')
+            ylabel('length of day (hours)')
+            subplot(2,1,2)
+            plot(T1,doy,'-ko')
+            xlabel('Age (Ma)')
+            ylabel('stellar day of year')
+        else
+            errordlg('Input age error !')
+        end
     end
 end
 
