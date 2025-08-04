79190165843a9530368bb9815c2c80a4aff46503
Mingsong Li <31803102+mingsongli@users.noreply.github.com>
2025-08-04 11:57:41 +0800

    acycle v3.0 debug

--- code/package/lyapunov/phase_space_reconstruct.m
+++
@@ -1,8 +0,0 @@
-function reconstructed = phase_space_reconstruct(time_series, dim, tau)
-    m = length(time_series) - (dim - 1) * tau;
-    reconstructed = zeros(m, dim);
-    for i = 1:dim
-        reconstructed(:, i) = time_series(1 + (i - 1) * tau : 1 + (i - 1) * tau + m - 1);
-    end
-    return;
-end
